5a578f5ec945700fd79844545b8862423cd979d6
mborisov011 <martin.borisov@cision.com>
2022-05-10 12:39:59 +0300

    test

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/dist/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+};
